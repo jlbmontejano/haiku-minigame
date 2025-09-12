53ef6d81826906aea44f43b4ad7d5dde62e55b41
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-13 09:45:00 +1200

    Fixed .env variables missing

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,12 @@
 import { configDotenv } from "dotenv";
+configDotenv();
+
 import app from "./src/app";
 
 async function main() {
-	configDotenv();
+	if (!process.env.GEMINI_API_KEY) {
+		throw new Error("API Key missing.");
+	}
 
 	app.listen(process.env.PORT, () => {
 		console.log("App listening on port: ", process.env.PORT);
