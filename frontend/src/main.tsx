c53175b8990a274ccda8f0cf94f734e64b61f9f0
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-14 08:22:38 +1200

    Added Vercel SpeedInsights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/context/theme-provider.tsx";
 import "@/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createRoot } from "react-dom/client";
 
 const queryClient = new QueryClient();
@@ -12,8 +13,9 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <AppProvider>
-                <Analytics />
                 <App />
+                <Analytics />
+                <SpeedInsights />
             </AppProvider>
         </ThemeProvider>
     </QueryClientProvider>,
