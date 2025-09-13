49bdf07777784ac3614288f9f198e80e0a46df7a
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-13 13:43:53 +1200

    Added Rating functionality

--- frontend/src/pages/Minigame/Minigame.tsx
+++ frontend/src/pages/Minigame/Minigame.tsx
@@ -1,16 +1,42 @@
+import Error from "@/components/Error";
+import Loader from "@/components/Loader";
+import RefetchButton from "@/components/RefetchButton";
+import { useAppContext } from "@/context/app-context";
+import { useHaikuRating } from "@/hooks/useHaikuRating";
+import { useGetHaiku } from "@/lib/react-query/haikuQueries";
 import Options from "./sections/Options";
-import SudokuDisplay from "./sections/SudokuDisplay";
+import RatingDisplay from "./sections/RatingDisplay";
+import HaikuDisplay from "./sections/HaikuDisplay";
 import Suggestions from "./sections/Suggestions";
 import UserInput from "./sections/UserInput";
 
 const Minigame = () => {
+    const { isRated } = useAppContext();
+    const { data: haiku, error, isFetching } = useGetHaiku();
+    const { handleRateHaiku, isPending } = useHaikuRating();
+
+    if (isFetching) return <Loader text="Getting your haiku..." />;
+    if (isPending) return <Loader text="AI is rating your haiku..." />;
+    if (error || !haiku) return <Error />;
+
+    const { text, suggestions, topic } = haiku;
+
     return (
-        <div className="width-control">
-            <SudokuDisplay />
-            <UserInput />
-            <Suggestions />
-            <Options />
-        </div>
+        <section className="width-control">
+            <HaikuDisplay wholeText={text} />
+            {isRated ? (
+                <>
+                    <RatingDisplay />
+                    <RefetchButton />
+                </>
+            ) : (
+                <>
+                    <UserInput handleRateHaiku={() => handleRateHaiku} />
+                    <Suggestions topic={topic} suggestions={suggestions} />
+                    <Options handleRateHaiku={() => handleRateHaiku(haiku)} />
+                </>
+            )}
+        </section>
     );
 };
 
