2ad471babcc1c3a876a336108876df1814a44c83
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-13 13:38:12 +1200

    Added toaster, moved getCurrentPage logic to a hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,20 @@
 import Footer from "@/components/Footer";
-import Loader from "@/components/Loader";
-import ModeToggle from "@/components/ModeToggle";
-import { useHaikuContext } from "@/context/haiku-context";
-import Minigame from "@/pages/Minigame/Minigame";
-import Rating from "@/pages/Rating/Rating";
-import Welcome from "@/pages/Welcome/Welcome";
-import { useEffect } from "react";
+import { Toaster } from "@/components/ui/sonner";
+import Header from "./components/Header";
+import { usePageRouter } from "./hooks/usePageRouter";
 
 function App() {
-    const { currentPage, isLoading, fetchHaiku } = useHaikuContext();
-
-    useEffect(() => {
-        fetchHaiku();
-    }, []);
-
-    const handleCurrentPage = () => {
-        switch (currentPage) {
-            case "welcome":
-                return <Welcome />;
-            case "minigame":
-                return isLoading ? <Loader /> : <Minigame />;
-            case "rating":
-                return <Rating />;
-            default:
-                return <Welcome />;
-        }
-    };
+    const getCurrentPage = usePageRouter();
 
     return (
-        <main className="main-container">
-            <ModeToggle />
-            {handleCurrentPage()}
-            <Footer />
-        </main>
+        <>
+            <main className="main-container">
+                <Header />
+                {getCurrentPage()}
+                <Footer />
+            </main>
+            <Toaster />
+        </>
     );
 }
 
