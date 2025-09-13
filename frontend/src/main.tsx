5a46bbe2f30012ce0f463ed0598ca98927c1b723
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-13 13:37:50 +1200

    Added react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import App from "@/App.tsx";
 import { ThemeProvider } from "@/context/theme-provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
+import { AppProvider } from "./context/app-context";
 import "./globals.css";
-import { HaikuProvider } from "./context/haiku-context";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <HaikuProvider>
-            <App />
-        </HaikuProvider>
-    </ThemeProvider>,
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <AppProvider>
+                <App />
+            </AppProvider>
+        </ThemeProvider>
+    </QueryClientProvider>,
 );
