c0ec510c248f227b5f6cd85d14fa2387da8d828e
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-13 13:42:15 +1200

    Added sipnner

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,7 +1,14 @@
-const Loader = () => {
+import { BounceLoader } from "react-spinners";
+
+type LoaderProps = {
+    text?: string;
+};
+
+const Loader = ({ text = "Loading..." }: LoaderProps) => {
     return (
-        <div className="width-control">
-            <p>Loading...</p>
+        <div className="width-control items-center" aria-label={text}>
+            <BounceLoader size="6rem" color="#828B81" />
+            <p className="subtitle">{text}</p>
         </div>
     );
 };
