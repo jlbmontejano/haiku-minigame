167b1b17231e0e36ec824ba002f37c5c82b90a8f
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-13 21:31:02 +1200

    Added vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { AppProvider } from "@/context/app-context";
 import { ThemeProvider } from "@/context/theme-provider.tsx";
 import "@/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/next";
 import { createRoot } from "react-dom/client";
 
 const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <AppProvider>
+                <Analytics />
                 <App />
             </AppProvider>
         </ThemeProvider>
