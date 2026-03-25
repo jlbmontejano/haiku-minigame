933d6473b8bc2182c2837ae8dd0dce70177d81ec
Jorge Laptop <jlbmontejano@gmail.com>
2026-03-25 19:56:06 +1300

    Adjusted footer text

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,17 @@
 const Footer = () => {
     return (
-        <footer className="flex justify-between text-sm lg:text-base">
-            <p>Created by Jorge Buenrostro</p>
-            <a
-                href="mailto:jlbmontejano@gmail.com"
-                rel="noreferrer"
-                className="hover:text-sage-green transition-all hover:underline"
-            >
-                Contact Me
-            </a>
+        <footer className="flex justify-end text-sm lg:text-base">
+            <p>
+                Created by{" "}
+                <a
+                    href="https://jorgebuenrostro.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hover:text-sage-green transition-all hover:underline"
+                >
+                    Jorge Buenrostro
+                </a>
+            </p>
         </footer>
     );
 };
